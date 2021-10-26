c308b037aaae9885d6a9d1e547bdcea2dd7d20a1
Tommaso Di Noto <tommydino@hotmail.it>
2021-10-26 10:04:12 +0200

    Update utils_strings.py

--- strings/utils_strings.py
+++ strings/utils_strings.py
@@ -16,3 +16,16 @@ def is_date(input_string: str, fuzzy: bool = False) -> bool:
 
     except ValueError:
         return False
+
+    
+def keep_only_digits(input_string: str) -> str:
+    """This function takes as input a string and returns the same string but only containing digits
+    Args:
+        input_string (str): the input string from which we want to remove non-digit characters
+    Returns:
+        output_string (str): the output string that only contains digit characters
+    """
+    numeric_filter = filter(str.isdigit, input_string)
+    output_string = "".join(numeric_filter)
+    
+    return output_string
