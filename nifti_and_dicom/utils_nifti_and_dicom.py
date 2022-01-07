cb706d4d03af07b20dfe963c89063470e76f143e
Tommaso Di Noto <tommydino@hotmail.it>
2022-01-07 12:55:38 +0100

    Update utils_nifti_and_dicom.py

--- nifti_and_dicom/utils_nifti_and_dicom.py
+++ nifti_and_dicom/utils_nifti_and_dicom.py
@@ -29,3 +29,49 @@ def resample_volume(volume_path: str, new_spacing: List, out_path, interpolator=
     os.remove(out_path)  # remove volume from disk to save space
 
     return resampled_volume_sitk_obj, resampled_volume_nii_obj, resampled_volume_nii
+
+
+def remove_zeros_ijk_from_volume(input_volume: np.ndarray) -> np.ndarray:
+    """This function removes all the rows, columns and slices of the input volume that only contain zero values.
+    Args:
+        input_volume (np.ndarray): volume from which we want to remove zeros
+    Returns:
+        cropped_volume (np.ndarray): cropped volume (i.e. input volume with zeros removed)
+    """
+    def remove_zeros_one_coordinate(input_volume_: np.ndarray, range_spatial_dim: int, spatial_dim: int):
+        idxs_nonzero_slices = []  # will the contain the indexes of all the slices that have nonzero values
+        for idx in range(range_spatial_dim):  # loop over coordinate
+            if spatial_dim == 0:
+                one_slice = input_volume_[idx, :, :]
+            elif spatial_dim == 1:
+                one_slice = input_volume_[:, idx, :]
+            elif spatial_dim == 2:
+                one_slice = input_volume_[:, :, idx]
+            else:
+                raise ValueError("spatial_dim can only be 0, 1, or 2. Got {} instead".format(spatial_dim))
+
+            if np.count_nonzero(one_slice) > 0:  # if the slice has some nonzero values
+                idxs_nonzero_slices.append(idx)  # save slice index
+
+        # retain only indexes with nonzero values from the two input volumes
+        if spatial_dim == 0:
+            cropped_volume_ = input_volume_[idxs_nonzero_slices, :, :]
+        elif spatial_dim == 1:
+            cropped_volume_ = input_volume_[:, idxs_nonzero_slices, :]
+        elif spatial_dim == 2:
+            cropped_volume_ = input_volume_[:, :, idxs_nonzero_slices]
+        else:
+            raise ValueError("spatial_dim can only be 0, 1, or 2. Got {} instead".format(spatial_dim))
+
+        return cropped_volume_
+
+    assert len(input_volume.shape) == 3, "The input volume must be 3D"
+
+    # i coordinate
+    cropped_volume = remove_zeros_one_coordinate(input_volume, input_volume.shape[0], spatial_dim=0)
+    # j coordinate
+    cropped_volume = remove_zeros_one_coordinate(cropped_volume, input_volume.shape[1], spatial_dim=1)
+    # k coordinate
+    cropped_volume = remove_zeros_one_coordinate(cropped_volume, input_volume.shape[2], spatial_dim=2)
+
+    return cropped_volume
