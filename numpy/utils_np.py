1ede50c512a25f2f674c219bf8eebefb11068a62
Tommaso Di Noto <tommydino@hotmail.it>
2022-01-13 15:18:49 +0100

    Create utils_np.py

---
+++ numpy/utils_np.py
@@ -0,0 +1,19 @@
+import numpy as np
+from typing import Any
+
+
+def find_most_frequent_value(input_array: np.ndarray) -> Any:
+    """This function finds the most common value in the input numpy array
+    Args:
+        input_array (np.ndarray): input array for which we want to find the most frequent value
+    Returns:
+        most_frequent_value (*): most frequent value
+    """
+    
+    values, counts = np.unique(input_array, return_counts=True)
+
+    idx = np.argmax(counts)
+    most_frequent_value = values[idx]  # extract the most frequent element
+
+    return most_frequent_value
+    
