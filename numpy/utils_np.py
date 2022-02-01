7d3aadc55d041f7639ef0d7e5de95ef032baa9a4
Tommaso Di Noto <tommydino@hotmail.it>
2022-02-01 15:05:31 +0100

    Update utils_np.py

--- numpy/utils_np.py
+++ numpy/utils_np.py
@@ -2,7 +2,7 @@ import numpy as np
 from typing import Any
 
 
-def find_most_frequent_value(input_array: np.ndarray):
+def find_most_frequent_value(input_array: np.ndarray) -> Any:
     """This function finds the most common value in the input numpy array
     Args:
         input_array (np.ndarray): input array for which we want to find the most frequent value
@@ -15,4 +15,18 @@ def find_most_frequent_value(input_array: np.ndarray):
     most_frequent_value = values[idx]  # extract the most frequent element
 
     return most_frequent_value
+    
+    
+def has_nans(input_array: np.ndarray) -> bool:
+    """"This function checks whether the input array contains any nan.
+    If yes, it returns True, otherwise it returns False.
+    Args:
+        input_array (np.ndarray): input array where we check for nans
+    Returns:
+        array_has_nan (bool): True if input_array contains nans; False otherwise
+    """
+    array_sum = np.sum(input_array)
+    array_has_nan = np.isnan(array_sum)
+    
+    return array_has_nan
     
