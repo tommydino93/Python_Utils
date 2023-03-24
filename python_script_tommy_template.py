4e72ab5ad5740269c847cafbf29416ca97cf9ad4
Tommaso Di Noto <tommydino@hotmail.it>
2023-03-24 18:45:08 +0100

    add template python script via upload

---
+++ python_script_tommy_template.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import re
+import pickle
+import subprocess
+import time
+import numpy as np
+import nibabel as nib
+import SimpleITK as sitk
+import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import datetime
+import cc3d
+import shutil
+import tkinter as tk
+import pydicom
+
+
+def function(ds_path):
+    return
+
+
+def main():
+    # input args
+    input_arg = "/path/to/smth/"
+
+    function(input_arg)
+
+
+if __name__ == '__main__':
+    main()
